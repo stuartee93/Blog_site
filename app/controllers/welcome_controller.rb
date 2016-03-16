23365e7e8b275fc6a7dba92229ab316c4cc0646b
Stuart Tee <Stuart1@Stuarts-MacBook-Pro.local>
2016-03-16 16:00:07 +0000

    added project page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
 	
 	def index
 		@posts = Post.all.limit(3).order("created_at DESC")
+		@projects = Project.all.limit(3).order("created_at DESC")
 	end
 
 end
